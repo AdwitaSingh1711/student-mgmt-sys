e560c9f9e5971f21467e83c0c7278139cd489e6e
AdwitaSingh1711 <108565358+AdwitaSingh1711@users.noreply.github.com>
2024-12-01 17:37:06 +0530

    debug

--- app/database.py
+++ app/database.py
@@ -3,9 +3,6 @@ import os
 
 # MongoDB Connection
 MONGO_URI = os.getenv("MONGO_URI")
-# MONGO_URI="mongodb+srv://adwitasat07:Gpt32w0cTqLKC9F8@cluster0.iwm2e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = AsyncIOMotorClient(MONGO_URI)
-# db = client.get_database("testdb")  # Replace "testdb" with your database name
-# collection = db["items"]
 db = client.get_database("student_management")
 collection = db["students"]
